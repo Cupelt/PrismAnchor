350d987b8d5549ac56c2635cba3b88409d61cf14
Cupelt <guswnd8800@naver.com>
2025-01-12 17:00:50 +0900

    Initializing on onLoad method.

--- PrismAnchorAPI/src/main/java/org/cupelt/prismanchor/AbstractPlugin.java
+++ PrismAnchorAPI/src/main/java/org/cupelt/prismanchor/AbstractPlugin.java
@@ -31,8 +31,6 @@ public abstract class AbstractPlugin<T extends AbstractPlugin<T>> extends JavaPl
                     CommandBuilder options = command.getCommandOptions();
                     getCommand(options.getName()).setExecutor(command);
                 });
-
-        initPlugin();
     }
 
     public abstract void initPlugin();
