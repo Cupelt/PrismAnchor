64db031c8d9c04e23da5ba5b9178b1d699be221d
Cupelt <guswnd8800@naver.com>
2025-01-12 21:15:48 +0900

    update Example Code

--- PrismAnchorExample/src/main/java/org/cupelt/prismanchor/example/PrismAnchorExample.java
+++ PrismAnchorExample/src/main/java/org/cupelt/prismanchor/example/PrismAnchorExample.java
@@ -3,8 +3,14 @@ package org.cupelt.prismanchor.example;
 import org.cupelt.prismanchor.AbstractPlugin;
 
 public final class PrismAnchorExample extends AbstractPlugin<PrismAnchorExample> {
+
     @Override
-    public void onEnable() {
+    public void onPluginEnable() {
         getLogger().info("Plugin allowed!");
+    }
+
+    @Override
+    public void onPluginDisable() {
+
     }
 }
