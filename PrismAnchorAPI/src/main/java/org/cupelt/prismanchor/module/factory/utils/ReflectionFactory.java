3d19ac70a63e3c9506a27a77b7e97539e58b39af
Cupelt <57065968+Cupelt@users.noreply.github.com>
2026-01-07 10:28:09 +0000

    fix: missing import

--- PrismAnchorAPI/src/main/java/org/cupelt/prismanchor/module/factory/utils/ReflectionFactory.java
+++ PrismAnchorAPI/src/main/java/org/cupelt/prismanchor/module/factory/utils/ReflectionFactory.java
@@ -1,5 +1,7 @@
 package org.cupelt.prismanchor.module.factory.utils;
 
+import org.cupelt.prismanchor.utils.ReflectionInitializer;
+
 public interface ReflectionFactory<T> {
     ReflectionInitializer<T> create(Class<T> clazz);
 }
