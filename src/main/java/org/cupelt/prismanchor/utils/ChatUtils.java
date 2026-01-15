c46d6d549af8974e3d178a7e921fc2cb7ed992e2
Cupelt <57065968+Cupelt@users.noreply.github.com>
2026-01-15 10:31:45 +0000

    feat: add minimessage resolver

--- src/main/java/org/cupelt/prismanchor/utils/ChatUtils.java
+++ src/main/java/org/cupelt/prismanchor/utils/ChatUtils.java
@@ -2,15 +2,28 @@ package org.cupelt.prismanchor.utils;
 
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.MiniMessage;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.cupelt.prismanchor.AbstractPlugin;
 
+import com.google.inject.Inject;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class ChatUtils {
+    @Inject
+    private static AbstractPlugin<?> plugin;
+
+    private static List<TagResolver> defaultResolvers = new ArrayList<>(Arrays.asList(
+        Placeholder.parsed("plugin", plugin.getPrefix())
+    ));
+
     public static String toColorString(String msg) {
         return ChatColor.translateAlternateColorCodes('&', msg);
     }
@@ -33,14 +46,20 @@ public class ChatUtils {
         player.sendMessage(comp);
     }
 
-    public static Component minimessage(String format, AbstractPlugin plugin) {
-        return MiniMessage.miniMessage().deserialize((plugin != null ? plugin.getPrefix() : "") + " " + format);
-    }
-
     public static Component minimessage(String format) {
-        return MiniMessage.miniMessage().deserialize(format);
+        return MiniMessage.miniMessage().deserialize(format, defaultResolvers.toArray(TagResolver[]::new));
     }
 
+    public static Component minimessage(String format, TagResolver... resolver) {
+        return MiniMessage.miniMessage().deserialize(
+            format, 
+            Stream.concat(
+                defaultResolvers.stream(), 
+                Stream.of(resolver)
+            ).toArray(TagResolver[]::new)
+        );
+    }
+    
     public static String ColorStringToMiniMessage(String colorString) {
         Component component = LegacyComponentSerializer.legacySection().deserialize(colorString);
         colorString = MiniMessage.miniMessage().serialize(component);
